d87ecb8ae43fd71b9d0c873a557efb4649457510
Hirotake Katayama <hirotakek@users.noreply.github.com>
2022-05-24 10:22:56 +0900

    Add files via upload

--- 002_Streamlit_sample_01.py
+++ 002_Streamlit_sample_01.py
@@ -314,9 +314,9 @@ def pdf_char():
     st.write(uploaded_file_conv1)
     
     if st.checkbox("変換実施"):
-        file_conv = open(uploaded_file_conv1,"rb")
+        # file_conv = open(uploaded_file_conv1,"rb")
         pdfparser = PDFParser()
-        reader = PyPDF2.PdfFileReader(file_conv)
+        reader = PyPDF2.PdfFileReader(uploaded_file_conv1)
         pageNo = reader.numPages()
         
         for i in range(pageNo):
