148fb743b67299543a5190c4420c1caa2e3a99bc
Hirotake Katayama <hirotakek@users.noreply.github.com>
2022-06-17 18:12:51 +0900

    Add files via upload

--- 002_Streamlit_sample_01.py
+++ 002_Streamlit_sample_01.py
@@ -345,8 +345,8 @@ def web_check():
     from selenium.webdriver.chrome.options import Options
     import time
 
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(ChromeDriverManager("102.0.5005.61").install())
+    options = webdriver.chrome.options.Options()
     options.add_argument('--headless')
     browser.get('https://www.yahoo.co.jp/')
     time.sleep(5)
