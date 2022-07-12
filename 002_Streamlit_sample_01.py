d817aea02919c0ede3892b17a80c80d428387340
Hirotake Katayama <hirotakek@users.noreply.github.com>
2022-07-12 15:46:17 +0900

    Add files via upload

--- 002_Streamlit_sample_01.py
+++ 002_Streamlit_sample_01.py
@@ -376,7 +376,7 @@ def web_check():
 
 def web_check01():
     import streamlit as st
-    from bs4 import BeautifulSoup
+    # from bs4 import BeautifulSoup
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     import chromedriver_binary
@@ -390,12 +390,12 @@ def web_check01():
     url = "https://google.com"
     driver.get(url)
     html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+    # soup = BeautifulSoup(html, 'html.parser')
 
     # 出力
-    ll = filter(lambda x: len(x) > 0, soup.text.split(" "))
-    for elem in ll:
-        st.write(elem)
+    # ll = filter(lambda x: len(x) > 0, soup.text.split(" "))
+    # for elem in ll:
+    #     st.write(elem)
 
 
 def web_table_get():
