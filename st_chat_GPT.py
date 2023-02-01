2e4f0dfeb026b3b860c5682bb30b5709bb4e4726
Hirotake Katayama <hirotakek@users.noreply.github.com>
2023-02-01 11:16:11 +0900

    Add files via upload

---
+++ st_chat_GPT.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 09:13:11 2023
+
+@author: hirkatayama
+"""
+
+import streamlit as st
+
+def main():
+    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
+    st.title("Chat GPT用")
+    #st.text_input("質問を記入して下さい。")
+    st.write('Hello, World!')
+
+
+
+
+if __name__ == '__main__':
+    main()
