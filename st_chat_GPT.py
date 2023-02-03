3510e5378bf3c2e3796a0b3b292a81989a2c1a58
Hirotake Katayama <hirotakek@users.noreply.github.com>
2023-02-03 13:57:46 +0900

    Add files via upload

--- st_chat_GPT.py
+++ st_chat_GPT.py
@@ -11,15 +11,17 @@ import streamlit as st
 # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
 st.header("Chat GPT用")
 #st.text_input("質問を記入して下さい。")
-st.caption('入力欄に質問等を書いてGoボタンを押して下さい。GPT-3が回答します。:')
-st.write('Googleの検索結果も欲しい場合は、Google検索ボタンを「ON」にして下さい。:  ')
-
+st.write('入力欄に質問等を書いてGoボタンを押して下さい。GPT-3が回答します。:')
+st.caption('Googleの検索結果も欲しい場合は、Google検索ボタンを「ON」にして下さい。:  ')
 google_clicked = st.radio("Google検索", ["On", "OFF"], horizontal=True)
 read_clicked = st.radio("読み上げ", ["OFF", "日本語", "English"], horizontal=True)
+in_text = st.text_area("入力欄")
+st.caption('入力に対する操作を指定して下さい。Go:チャット、Clear:消去、Exit:処理終了')
+
 go_clicked = st.button("Go")
 clear_clicked = st.button("Clear")
 exit_clicked = st.button("Exit")
-in_text = st.text_area("入力欄")
+st.caption('以下の出力欄に、GPT-3の結果、指定によりGoogleと併せて表示されます。')
 out_text = st.text_area("出力欄")
 
 
