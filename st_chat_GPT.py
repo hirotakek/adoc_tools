16ce59f8c3ee472712b66eff81aa202b24932f06
Hirotake Katayama <hirotakek@users.noreply.github.com>
2023-02-03 13:37:04 +0900

    Add files via upload

--- st_chat_GPT.py
+++ st_chat_GPT.py
@@ -14,7 +14,7 @@ st.header("Chat GPT用")
 st.caption('入力欄に質問等を書いてGoボタンを押して下さい。GPT-3が回答します。:')
 st.write('Googleの検索結果も欲しい場合は、Google検索ボタンを「ON」にして下さい。:  ')
 
-google_clicked = st.radio("Google検索", ["On", "OFF"], horizontal=False)
+google_clicked = st.radio("Google検索", ["On", "OFF"], horizontal=True)
 read_clicked = st.radio("読み上げ", ["日本語", "OFF", "English"])
 go_clicked = st.button("Go")
 clear_clicked = st.button("Clear")
