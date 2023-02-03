20afa751ff9a6a11fe8ab7e80cc680a7ca27fdc6
Hirotake Katayama <hirotakek@users.noreply.github.com>
2023-02-03 11:50:50 +0900

    Add files via upload

--- st_chat_GPT.py
+++ st_chat_GPT.py
@@ -7,25 +7,26 @@ Created on Fri Jan 27 09:13:11 2023
 
 import streamlit as st
 
-def main():
-    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
-    st.title("Chat GPT用")
-    #st.text_input("質問を記入して下さい。")
-    st.subtitle('入力欄に質問等を書いてGoボタンを押して下さい。GPT-3が回答します。:')
-    st.write('Googleの検索結果も欲しい場合は、右のボタンを「ON」にして下さい。:  ')
+# def main():
+# Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
+st.title("Chat GPT用")
+#st.text_input("質問を記入して下さい。")
+st.subtitle('入力欄に質問等を書いてGoボタンを押して下さい。GPT-3が回答します。:')
+st.write('Googleの検索結果も欲しい場合は、右のボタンを「ON」にして下さい。:  ')
 
-    google_clicked = st.radio("Google検索", ["On", "OFF"])
-    read_clicked = st.radio("読み上げ", ["日本語", "OFF", "English"])
-    go_clicked = st.button("Go")
-    clear_clicked = st.button("Clear")
-    exit_clicked = st.button("Exit")
-    in_text = st.text_area("入力欄")
-    out_text = st.text_area("出力欄")
+google_clicked = st.radio("Google検索", ["On", "OFF"])
+read_clicked = st.radio("読み上げ", ["日本語", "OFF", "English"])
+go_clicked = st.button("Go")
+clear_clicked = st.button("Clear")
+exit_clicked = st.button("Exit")
+in_text = st.text_area("入力欄")
+out_text = st.text_area("出力欄")
+
+
     
 
-    
 
-
-
+"""
 if __name__ == '__main__':
     main()
+"""
